071266c650b54712651081581489ae28c444732f
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-17 04:09:16 -0800

    combinations of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: always 0
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (j != i)
+			{
+				putchar(i);
+				putchar(j);
+				if (ij != '89')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
