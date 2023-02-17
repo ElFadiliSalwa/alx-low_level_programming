3745b88f5481086a55211ed376ed16ccbbbd33ee
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-17 06:51:51 -0800

    Numbers separated by comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,12 @@ int main(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-		if (i == 57)
-			putchar('9');
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
