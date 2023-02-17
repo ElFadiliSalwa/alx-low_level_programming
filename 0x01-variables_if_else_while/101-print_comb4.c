00b05d67dc63955b4ea75c59bb6b19ca4a5f32ec
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-17 05:25:43 -0800

    Combination of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: always 0
+*/
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 55; i++)
+	{
+		for (j = 49; j<= 56; j++)
+		{
+			for (k = 50; k <= 57; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i == 55 && j == 56 && k == 57)
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
