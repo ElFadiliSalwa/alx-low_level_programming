532de64e8e8b9d3ec10cc833465958900ab5acac
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-17 03:11:46 -0800

    Alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: always 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a', c <= 'z'; c++)
+	{
+		while (c != 'e' && c!= 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
