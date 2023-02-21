2e49cf704110414e54dd77c6e26043911fcc1a99
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-21 12:50:27 -0800

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,14 +21,24 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar(p + '0');
 				}
-				else if (p >= 10)
+				else if (p >= 10 && p < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar(p / 10 + '0');
 					_putchar(p % 10 + '0');
+				}
+				else if (p >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(p / 100 + '0');
+					_putchar((p / 10) % 10 + '0');
+					=putchar(p % 10 + '0');
 				}
 			}
 			_putchar('\n');
