4777f04048fcdd760ea96204aa3fcb4d3bf2ac3c
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-21 10:12:59 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,18 +15,20 @@ void times_table(void)
 			{
 				_putchar(p / 10 + '0');
 				_putchar(p % 10 + '0');
+				if (j == 9)
+					continue;
 				_putchar(',');
 				_putchar(' ');
 			}
 			else
 			{
 				_putchar(p + '0');
+				if (j == 9)
+					continue;
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 			}
-			if (j == 9)
-				continue;
 		}
 		_putchar('\n');
 	}
