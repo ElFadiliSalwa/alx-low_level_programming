d9422afbb1d7b4939c7d577397d6746ba9384050
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-21 13:17:18 -0800

    Sum of natural numbers below 1024 and multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main (void)
+{
+	int i;
+	long int j = 1, k = 2;
+
+	for (i = 0; i < 50; ++i)
+	{
+		if (i == 0)
+			printf("%ld", j);
+		else if (i == 1)
+			printf(", %ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+	}
+	printf("\n");
+	return (0);
+}
