d67c6619429461b898ea3aa89f7392e2669de10b
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-21 12:43:32 -0800

    Times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table, starting from 0
+ * @n: the starting number of times table
+ */
+void print_times_table(int n)
+{
+	int i, j, p;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= 9; j++)
+			{
+				p = i * j;
+				if (j == 0)
+					_putchar(p + '0');
+				if (p < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(p + '0');
+				}
+				else if (p >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(p / 10 + '0');
+					_putchar(p % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
