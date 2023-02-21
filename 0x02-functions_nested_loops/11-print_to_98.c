2d226e2e389fb1b854d1854753a73ba57984d294
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-21 11:51:39 -0800

    From n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,12 +8,21 @@ void print_to_98(int n)
 	int i;
 
 	if (n == 98)
-		_putchar(n + '0');
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
 	if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			_putchar(i + '0');
+			if (i < 10)
+				_putchar(i + '0');
+			else
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
 			if (i != 98)
 			{
 				_putchar(',');
@@ -25,7 +34,15 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			_putchar(i + '0');
+			if (i < 10)
+				_putchar(i + '0');
+			else if (i < 100)
+			{
+				ten = (i % 100) / 10;
+				_putchar(i / 100 + '0');
+				_putchar(ten + '0');
+				_putchar(i % 10 + '0');
+			}
 			if (i != 98)
 			{
 				_putchar(',');
