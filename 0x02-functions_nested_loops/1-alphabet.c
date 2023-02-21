9990e51d3cc52e3b46e6adf9eaf8ada5efb32b92
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-21 07:15:19 -0800

    Print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 97; c < 122; c++)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}
