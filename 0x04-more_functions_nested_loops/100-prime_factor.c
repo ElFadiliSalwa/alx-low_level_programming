b8736f17325fbcb374a5faeae6febae659e727b3
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-23 07:54:48 -0800

    The largest prime factor of a number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,18 +7,17 @@
  */
 int main(void)
 {
-	long int n = 612852475143;
+	long n = 612852475143;
 	int div = 2, max = 0;
 
 	while (n != 1)
 	{
-		if (n % div != 0)
-			div +=  1;
-		else
+		if (n % div == 0)
 		{
 			n /= div;
 			max = div;
 		}
+		div += 1;
 	}
 	printf("%ld\n", max);
 	return (0);
