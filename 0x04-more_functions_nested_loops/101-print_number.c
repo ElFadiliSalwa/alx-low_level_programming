f75f3d2cf9a2aa7f7fdcbfc87fa6b47503984da3
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-23 08:52:57 -0800

    Print intengers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,12 +6,15 @@
  */
 void print_number(int n)
 {
+	int positive;
+
+	positive = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		positive = -n;
 	}
-	if (n / 10 != 0)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (positive / 10 != 0)
+		print_number(positive / 10);
+	_putchar(positive % 10 + '0');
 }
