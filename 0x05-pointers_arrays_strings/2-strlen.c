ecc1092fbaec561998ba6e20dd3a3c40d2306b9f
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-27 11:55:48 -0800

    Calculate the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to calculate its length
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	return sizeof(*s);
+}
