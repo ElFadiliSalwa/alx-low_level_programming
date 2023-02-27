493bbba469e2f8abefc2583574c2e9e7a268ea05
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-27 12:49:46 -0800

    Print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,7 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = _strlen(*s); i >= 0; i--)
-		putchar(s[i]);
-	putchar('\n');
+	for (i = _strlen(s); i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
