c9856cfc30a8e561c493aa8a7f31608319e6614c
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-27 16:15:37 -0800

    Copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,10 +12,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (i <  _strlen(src))
+	while (i <=  _strlen(src))
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	return dest;
+	return (dest);
 }
