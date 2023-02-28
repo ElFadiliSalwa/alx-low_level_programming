c7297188958490678dac7739a70da68c0ffef038
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-02-27 16:12:25 -0800

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strcpy - copies the string pointed to
+ * @dest: destination
+ * @src: source
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (i <  _strlen(src))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+}
