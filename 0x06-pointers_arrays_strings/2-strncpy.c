bb55603b6effff3ba919a7fc0f2889a1d42c4aa2
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-01 14:17:32 -0800

    Copy strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (i < n)
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-		i++;
-	}
+	for (i = 0; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
