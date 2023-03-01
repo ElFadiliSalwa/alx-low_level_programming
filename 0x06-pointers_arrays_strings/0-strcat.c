71a5e3160152b89ade68c55f8ffa1ba6dc6fecfc
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-01 13:02:35 -0800

    Concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,10 +11,14 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-    	for (i = 0; dest[i] != '\0'; i++)
-   	{}
-    	for (j = 0; src[j] != '\0'; j++)
-        	dest[i + j] = src[j];
-	dest[i + j] = '\0';
+	for (j = 0; dest[j] != '\0'; j++)
+	{}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
+
 	return (dest);
 }
