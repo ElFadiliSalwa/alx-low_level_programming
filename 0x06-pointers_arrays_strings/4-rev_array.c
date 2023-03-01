098366169df625f22120969889deb004998aecae
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-01 14:42:13 -0800

    Reverse array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,10 +6,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j, tmp[n];
+	int i, tmp;
 
-	for (i = 0, j = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		tmp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = tmp;
 	}
 }
