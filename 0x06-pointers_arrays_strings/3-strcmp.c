eb2f856bdf58710385a55c99d0f2e10694bc615b
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-01 14:26:40 -0800

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,21 +10,9 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0, dif;
+	int i;
 
-        while (i <= strlen(s1) && j <= strlen(s2))
-        {
-            if (s1[i] == s2[j])
-            {
-                i++;
-                j++;
-                dif = 0;
-            }
-            else
-            {
-                dif = s1[i] - s2[j];
-                break;
-            }
-        }
-        return dif;
+	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
+		i++;
+        return (s1[i] - s2[i]);
 }
