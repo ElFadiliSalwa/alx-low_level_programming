d24176b2bf78ce52c4b568a7fd9ba058c4636050
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-02 14:51:21 -0800

    Encode a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 -  encodes a string using rot13
+ * @str: the string to encode
+ *
+ * Return: char
+ */
+char *rot13(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		while ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 90 && str[i] <= 122))
+		{
+			if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 90 && str[i] <= 122))
+				str[i] += 13;
+			else
+				str[i] -= 13;
+			i++;
+		}
+		i++;
+	}
+	return (str);
+}
