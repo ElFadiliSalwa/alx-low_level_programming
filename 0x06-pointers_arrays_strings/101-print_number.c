afc939816922aa0e82751ecb258dbf185a5e58dc
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-01 16:34:48 -0800

    Print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int positive;
+
+	positive = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		positive = -n;
+	}
+	if ((positive / 10) != 0)
+		print_number(positive / 10);
+	_putchar((positive % 10) + '0');
+}
