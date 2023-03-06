23b75770924c928b67ba18141cfaf0d29acc52b0
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-06 07:28:07 -0800

    Locate a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,10 @@
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
-        {
-            if (c == *s)
-                return (s);
-            s++;
-        }
-        return NULL;
+	{
+		if (c == *s)
+			return (s);
+		s++;
+	}
+	return NULL;
 }
