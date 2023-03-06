6c2e884b099bae478dd985cf5fc74968ffc087b7
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-06 11:46:49 -0800

    Locate a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,16 +10,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	char *tmp_haystack, *tmp_needle;
+
 	while (*haystack)
 	{
-		while (*needle && *haystack == *needle)
+		tmp_haystack = haystack;
+		tmp_needle = needle;
+		while (*tmp_needle && *haystack == *tmp_needle)
 		{
 			haystack++;
-			needle++;
+			tmp_needle++;
 		}
+		if (!*tmp_needle)
+			return (tmp_haystack);
 		haystack++;
-		if (!*needle)
-			return (haystack);
 	}
 	return ('\0');
 }
