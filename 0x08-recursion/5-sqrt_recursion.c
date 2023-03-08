55859fd6c89d46b01aa50ce7a94421fea8a9c763
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-08 06:51:06 -0800

    The square of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int _square_of_number(int n, int root);
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number
+ *
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_square_of_number(n, 0));
+}
+
+/**
+ * _square_of_number - finds the natural
+ * @n: the number
+ * @root: iterator
+ *
+ * Return: int
+ */
+
+int _square_of_number(intn, int root)
+{
+	if (root * root > n)
+		return (-1);
+	if (root * root == n)
+		return (root);
+	return (_square_of_number(n, root + 1));
+}
