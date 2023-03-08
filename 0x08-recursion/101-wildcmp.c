1f0b9cab6adfabe502c5eaaa0fa513fcac16bc8a
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-08 08:27:56 -0800

    Compare two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,8 +15,8 @@ int wildcmp(char *s1, char *s2)
 	if (!*s1 && *s2 == '*' && s2[1])
 		return (0);
 	if (*s1 == *s2)
-		return (wildcmp(s1++, s2++));
+		return (wildcmp(++s1, ++s2));
 	if (*s2 == '*')
-		return (wildcmp(s1, s2++) || wildcmp(s1++, s2));
+		return (wildcmp(s1, ++s2) || wildcmp(++s1, s2));
 	return (0);
 }
