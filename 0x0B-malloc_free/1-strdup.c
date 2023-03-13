9e733a74e11f65198e6f4e7284d988fb0cb40398
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-13 12:49:11 -0700

    Return a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: pointer to a string
+ *
+ * Return: pointer to a string
+ */
+
+char *_strdup(char *str)
+{
+	char *copy;
+	unsigned int i = 0, j = 0;
+
+	if (!str)
+		return (NULL);
+
+	while (*str)
+		i++;
+
+	copy = malloc(sizeof(char) * (i + 1));
+
+	if (!copy)
+		return (NULL);
+
+	while (j <= i)
+	{
+		copy[j] = str[j];
+		j++;
+	}
+	return (copy);
+}
