67ea31d31c9075131614d53e381245ba8bda4a01
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-13 12:34:58 -0700

    Create and initialize an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,8 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	if (size == 0)
-		return (NULL);
-	
+		return (NULL);	
 	t = malloc(size);	
 	if (t == NULL)
 		return (NULL);
