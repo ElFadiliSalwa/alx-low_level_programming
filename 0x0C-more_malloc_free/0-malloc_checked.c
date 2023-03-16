7cabbccf23fceece9ca30b8abf6373e7cc505134
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-16 12:11:33 -0700

    Allocating memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: size of memory to be allocated
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if(ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
