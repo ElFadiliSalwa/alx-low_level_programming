1cc6f66c60d48f8faaa208c360b1dbea90ac13d0
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-20 17:39:05 -0700

    Create a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,71 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: the string
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * _strcpy - copies a string
+ * @dst: the destination
+ * @src: the source
+ *
+ * Return: pointer to a char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: new name
+ * @age: new age
+ * @owner: new owner
+ *
+ * Return: pointer to a struct dog type
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	int size1, size2;
+
+	size1 = _strlen(name);
+	size2 = _strlen(owner);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	d->name = malloc(sizeof(char) * (size1 + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->owner = malloc(sizeof(char) * (size2 + 1));
+	if (d->owner == NULL)
+	{
+		free(d);
+		free(d->name);
+		return (NULL);
+	}
+	_strcpy(d->name, name);
+	_strcpy(d->owner, owner);
+	d->age = age;
+	return (d);
+}
