d45d2b6c28cef9135f4933e2a421ef3915afb6b6
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-22 17:07:29 -0700

    Execute a function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,7 +4,7 @@
 /**
  * array_iterator - executes a function
  * @array: the array
- * @@size: the size of the array
+ * @size: the size of the array
  * @action: pointer to function
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
