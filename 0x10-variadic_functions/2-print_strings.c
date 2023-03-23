4fbc104dac587f8e5da61e352cc1d7ebd614225e
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-23 10:56:49 -0700

    Prints the strings passed on parameters

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,12 +10,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
+	char *s = "";
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		if (va_arg(ap, char*) != NULL)
-			printf("%s", va_arg(ap, char*));
+		{
+			s = va_arg(ap, char*);
+			printf("%s", s);
+		}
 		else
 			printf("(nil)");
 		if (separator != NULL)
