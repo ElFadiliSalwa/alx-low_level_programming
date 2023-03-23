346c939a621f1e8a1ea2b4f031a35b62400f5d0e
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-03-23 10:34:43 -0700

    Print all the numbers passed as parameters

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,14 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
+	if (separator == NULL)
+		separator = "";
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (separator != NULL)
-		{
-			if (i < n - 1)
+		if (i < n - 1)
 				printf("%s", separator);
-		}
 	}
 	printf("\n");
 	va_end(ap);
