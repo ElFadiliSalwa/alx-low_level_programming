6c4106aed8a384483932b93ce42825d322a1192e
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-04-10 18:20:20 -0700

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	fw = write(fp, text_content, txt_len);
-	if (fp == -1 || fw == -1)
+	if (fp == -1 || fw == -1 || fw != txt_len)
 		return (-1);
 
 	close(fp);
