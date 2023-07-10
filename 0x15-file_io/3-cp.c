951b33f3304b38f84800fe0bac7649474f2f9410
ElFadiliSalwa <salwa.el.fadili.salwa@gmail.com>
2023-07-10 13:09:38 +0100

    Task 3: Copying the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,62 +1,81 @@
 #include "main.h"
 
-/**
-* main - copies the content of a file to another file
-* @argc: argument count
-* @argv: argument vector
-*
-* Return: Always 0
-*/
+#define MAXSIZE 1024
 
+
+/**
+ * __exit - prints error messages and exits with exit number
+ *
+ * @err: the exit number or file descriptor
+ * @s: name of file
+ * @f: file descriptor
+ *
+ * Return: Always 0
+*/
+int __exit(int err, char *s, int f)
+{
+	switch (err)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			exit(err);
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+			exit(err);
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+			exit(err);
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f;
+			exit(err);
+		default:
+			return (0);
+	}
+}
+
+/**
+ * main - copies the content of a file to another file
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+*/
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-	int size1 = 1024, size2 = 0;
-	char buffer[1024];
+	int file_in, file_out;
+	int read_stat, write_stat;
+	int close_in, close_out;
+	char buffer[MAXSIZE];
 
 	if (argc != 3)
+		__exit(97, NULL, 0);
+
+	file_in = open(argv[1], O_RDONLY);
+	if (file_in == -1)
+		__exit(98, argv[1], 0);
+
+	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_out == -1)
+		__exit(99, argv[2], 0);
+
+	while ((read_stat = read(file_in, buffer, MAXSIZE)) != 0)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(file_from);
-		exit(99);
-	}
-	while (size1 == 1024)
-	{
-		size1 = read(file_from, buffer, 1024);
-		if (size1 == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		size2 = write(file_to, buffer, size1);
-		if (size2 < size1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+		if (read_stat == -1)
+			__exit(98, argv[1], 0);
+
+		write_stat = write(file_out, buffer, read_stat);
+		if (write_stat == -1)
+			__exit(99, argv[2], 0);
 	}
 
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+	close_in = close(file_in);
+	if (close_in == -1)
+		__exit(100, NULL, file_in);
+
+	close_out = close(file_out);
+	if (close_out == -1)
+		__exit(100, NULL, file_out);
+
 	return (0);
 }
